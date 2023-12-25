2312a9e352cec925411979e14198db7f1d339f4c
lelam120 <79979521+lelam120@users.noreply.github.com>
2023-12-26 00:06:38 +0700

    Add files via upload

---
+++ 27.12/BTTL/BT1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+		 
+	  int n;
+	  printf("nhap vao so dong:");
+	  scanf("%d",&n);
+      
+      for (int i=1;i<=n;i++){
+      	for (int j=1;j<=5;j++){
+      	printf ("*");
+	  }  
+	  printf ("\n");
+	}
+}	  
+
---
+++ 27.12/BTTL/BT2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+		 
+	  int n;
+	  printf("nhap vao so dong:");
+	  scanf("%d",&n);
+      
+      for (int i=1;i<=n;i++){
+      	for (int j=1;j<=5;j++){
+      	printf ("*");
+	  } 
+	  printf("\n");
+	    for (int g=1;g<=4;g++){
+      	printf ("*");
+      } 
+	  printf("\n");
+}	  
+}	  
+
